3f9b5a3e2982017edc3cedb54f7202ecb94d694b
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-07-19 11:12:05 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# higgs
+
+Set Github actions secrets:
+
+- PERSONAL_TOKEN (also for dependabot)
+- SERVER_CONFIG_TOKEN (server-config token)
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { logout, start_auth, test_token } from '$lib/backend/auth.svelte';
+
+  const login = async () => {
+    let res = await start_auth();
+    if (res) {
+      window.location.href = res;
+    }
+  };
+
+  let valid = $state();
+  const test = async () => {
+    valid = await test_token();
+  };
+</script>
+
+<button onclick={login} class="text-white"> Login </button>
+<button onclick={logout} class="text-white"> Logout </button>
+<button onclick={test} class="text-white"> Test </button>
+<p class="text-white">Valid: {valid}</p>
